1ad5e607cd630a919c010dbedd5821ca1515966e
Roman <romanbuk52@gmail.com>
2023-04-09 20:45:03 +0000

    added configs for docker

---
+++ links_list.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+ln -s /root/general_configs/system_configs/nginx.conf /etc/nginx/nginx.conf
+ln -s /root/general_configs/system_configs/smb.conf /etc/samba/smb.conf
+ln -s /hdd/docker /var/lib/docker 
